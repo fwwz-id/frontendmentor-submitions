adc2b30369307a15abb6a1ad6e133defbb72390a
Fawwaz <63389991+fawwaz-abdurrahim@users.noreply.github.com>
2021-11-10 05:04:55 +0700

    Update rest countries API url

--- rest-countries-api/src/countries.api.js
+++ rest-countries-api/src/countries.api.js
@@ -1,5 +1,5 @@
 export const Countries = async (param) => {
-	const data = await fetch(`https://restcountries.eu/rest/v2${param}`);
+	const data = await fetch(`https://restcountries.com/rest/v2${param}`);
 	const response = await data.json();
 
 	return response;
